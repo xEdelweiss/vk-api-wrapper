6d4423f32441cc9cc9a9311816612edca1fedd82
Michael Sverdlikovsky <michael.sverdlikovsky@ab-soft.net>
2015-09-02 13:21:06 +0300

    Countries component: get countries by code

--- src/Component/Countries.php
+++ src/Component/Countries.php
@@ -10,10 +10,10 @@ class Countries extends BasicComponent
     static $prefix = 'database';
 
     /**
-     * @param null $needAll
-     * @param null $code
-     * @param null $count
-     * @param null $offset
+     * @param bool|null $needAll
+     * @param array|null $code
+     * @param integer|null $count
+     * @param integer|null $offset
      * @return \VkApi\Response\CountriesListResponse
      * @throws \Exception
      * @throws \VkApi\Exception\Api\TooManyRequestsException
@@ -33,7 +33,7 @@ class Countries extends BasicComponent
     }
 
     /**
-     * @param $ids
+     * @param array $ids
      * @return \VkApi\Response\CountriesListResponse
      * @throws \Exception
      * @throws \VkApi\Exception\Api\TooManyRequestsException
@@ -50,12 +50,34 @@ class Countries extends BasicComponent
     }
 
     /**
-     * @param $id
+     * @param array $codes
+     * @param integer|null $count
+     * @param integer|null $offset
+     * @return CountriesListResponse
+     */
+    public function getCountriesByCode($codes, $count = null, $offset = null)
+    {
+        return $this->get(null, $codes, $count, $offset);
+    }
+
+    /**
+     * @param int $id
      * @return \VkApi\Entity\Country
      */
     public function getCountry($id)
     {
         $result = $this->getCountriesById([$id]);
+
+        return $result->getFirstItem();
+    }
+
+    /**
+     * @param string $code
+     * @return \VkApi\Entity\Country
+     */
+    public function getCountryByCode($code)
+    {
+        $result = $this->getCountriesByCode([$code]);
 
         return $result->getFirstItem();
     }
